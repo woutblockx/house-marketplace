ad26dbb5dbbd16b1dd303e3f194df9beb8c2e20e
Wout Blockx <woutblockxx@gmail.com>
2023-08-17 14:46:40 +0200

    Part 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
-    <div>
-    <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <NavBar />
+      </Router>
+    </>
   );
 }
 
